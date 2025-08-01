25b729514f4421505367e7449a1cf2ccaf439c9e
Waltinho <walterpotma@gmail.com>
2025-08-01 11:22:52 -0300

    Update Program.cs

--- Orbit.Api/Orbit.Api/Program.cs
+++ Orbit.Api/Orbit.Api/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(7000); // Mesma porta exposta no Dockerfile
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
