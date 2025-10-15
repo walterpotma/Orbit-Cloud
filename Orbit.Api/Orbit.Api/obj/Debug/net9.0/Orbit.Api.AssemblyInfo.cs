b66b78458f7134afceb35a09e0059cc22c3ca3f8
Waltinho <walterpotma@gmail.com>
2025-10-15 08:35:26 -0300

    Add credenciais do google auth

--- Orbit.Api/Orbit.Api/obj/Debug/net9.0/Orbit.Api.AssemblyInfo.cs
+++ Orbit.Api/Orbit.Api/obj/Debug/net9.0/Orbit.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orbit.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d886508837b134fb4e4bfcec2f9d4f0b7c431934")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15878e687d1fc76894654e019d6e032231291ec2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orbit.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orbit.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
