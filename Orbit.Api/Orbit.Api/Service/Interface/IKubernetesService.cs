e69150ba1514ecc8d9e350e06e3ccfe01a09f560
Waltinho <walterpotma@gmail.com>
2025-10-30 21:46:23 -0300

    /Orbit.Api/: Arquivo restante para a Feature Kubernetes Secret

--- Orbit.Api/Orbit.Api/Service/Interface/IKubernetesService.cs
+++ Orbit.Api/Orbit.Api/Service/Interface/IKubernetesService.cs
@@ -8,11 +8,12 @@ namespace Orbit.Api.Service.Interface
         Task<IEnumerable<DtoService>> GetAllServicesAsync(string? namespaceName = null);
         Task<IEnumerable<DtoIngress>> GetAllIngressesAsync(string? namespaceName = null);
 
-
+        #region Kubernetes Secret
         Task<IEnumerable<DtoSecretResponse>> GetAllSecretsAsync(string? namespaceName = null);
         Task<DtoSecretResponse> GetSecretsAsync(string name, string namespaces);
         Task<DtoSecretResponse> CreateSecretsAsync(DtoSecretRequest request, string namespaces);
         Task DeleteSecretsAsync(string name, string namespaces);
+        #endregion
 
         #region Kubernetes Namespace
         Task<IEnumerable<DtoNamespaceResponse>> GetAllNamespacesAsync();
