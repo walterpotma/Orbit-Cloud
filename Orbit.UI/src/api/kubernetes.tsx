0c859f3218ca2c6926c34a0859048fda24d9e5fc
Waltinho <walterpotma@gmail.com>
2025-12-25 20:06:30 -0300

    Update kubernetes.tsx

--- Orbit.UI/src/api/kubernetes.tsx
+++ Orbit.UI/src/api/kubernetes.tsx
@@ -3,13 +3,13 @@ import { axiosInstance } from "./config";
 const controller = "kubernetes";
 
 export class Pods {
-    static List() {
-        return axiosInstance.get(`${controller}/pods`, { withCredentials: true });
+    static List(namespace?: string) {
+        return axiosInstance.get(`${controller}/pods/${namespace || ""}`, { withCredentials: true });
     }
 }
 
 export class Deployments {
-    static List() {
-        return axiosInstance.get(`${controller}/deployments`, { withCredentials: true });
+    static List(namespace?: string) {
+        return axiosInstance.get(`${controller}/deployments/${namespace || ""}`, { withCredentials: true });
     }
 }
