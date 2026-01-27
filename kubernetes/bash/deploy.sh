8f1260f0545eecaa4a399ed30a5aadddb5519ee8
Waltinho <walterpotma@gmail.com>
2026-01-27 19:03:32 -0300

    Refactor deploy script to remove namespace parameter and update echo messages

--- kubernetes/bash/deploy.sh
+++ kubernetes/bash/deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 APP_NAME=$1
-APP_FILE=$2
-NAMESPACE=$3
-VERSION=$4
+VERSION=$2
+APP_FILE=$3
 
-echo "[SH] Iniciando deploy para $APP_NAME na namespace $NAMESPACE..."
+echo "[SH] Iniciando deploy para $APP_NAME"
 
 docker build -t localhost:5000/$APP_NAME:v$VERSION ../../$APP_FILE
 if [ $? -ne 0 ]; then
@@ -17,9 +16,9 @@ if [ $? -ne 0 ]; then
     echo "[SH] Erro ao enviar a imagem Docker para o registro."
     exit 1
 fi
-kubectl apply -n $NAMESPACE -f ../$APP_NAME.yaml
+kubectl apply -f ../$APP_NAME.yaml
 if [ $? -eq 0 ]; then
-    echo "[SH] Sucesso! Deploy aplicado para $APP_NAME na namespace $NAMESPACE"
+    echo "[SH] Sucesso! Deploy aplicado para $APP_NAME"
     exit 0
 else
     echo "[SH] Erro ao aplicar o deploy."
