937d25f5e617de3126355d0f67c39bb175fd52f4
Waltinho <walterpotma@gmail.com>
2026-01-28 22:38:53 -0300

    Alterando deploy.sh

--- kubernetes/bash/deploy.sh
+++ kubernetes/bash/deploy.sh
@@ -6,17 +6,24 @@ APP_FILE=$3
 
 echo "[SH] Iniciando deploy para $APP_NAME"
 
-docker build -t localhost:5000/$APP_NAME:v$VERSION $APP_FILE
+# --- MUDANÇA AQUI: Adicionei DOCKER_BUILDKIT=1 ---
+DOCKER_BUILDKIT=1 docker build -t localhost:5000/$APP_NAME:v$VERSION $APP_FILE
+# -------------------------------------------------
+
 if [ $? -ne 0 ]; then
     echo "[SH] Erro ao construir a imagem Docker."
     exit 1
 fi
+
 docker push localhost:5000/$APP_NAME:v$VERSION
+
 if [ $? -ne 0 ]; then
     echo "[SH] Erro ao enviar a imagem Docker para o registro."
     exit 1
 fi
+
 kubectl apply -f ../deployments/$APP_NAME.yaml
+
 if [ $? -eq 0 ]; then
     echo "[SH] Sucesso! Deploy aplicado para $APP_NAME"
     exit 0
