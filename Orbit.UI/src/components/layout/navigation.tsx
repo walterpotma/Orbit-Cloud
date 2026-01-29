1750d26c10abf40f66f7cc94240d7535bb7bc519
walter potma <145157901+waltindobne@users.noreply.github.com>
2026-01-29 10:37:03 -0300

    Organizando Arquivos e codigo para nova arquitetura

--- Orbit.UI/src/components/layout/navigation.tsx
+++ Orbit.UI/src/components/layout/navigation.tsx
@@ -35,26 +35,26 @@ export default function Nav() {
     };
 
     return (
-        <nav className={`${pathUrl == "/login" || pathUrl == "/register" ? "hidden" : ""} w-${width} min-w-${width} pt-5 h-full bg-[var(--dark)] relative transition duration-300 ease-in-out`}>
+        <nav className={`${pathUrl == "/login" || pathUrl == "/register" ? "hidden" : ""} w-${width} min-w-${width} pt-5 h-full bg-[var(--dark)] flex flex-col relative transition duration-300 ease-in-out`}>
             <h1 className={`w-full flex justify-center items-center`}>
                 <Logo />
                 <p className={`text-3xl font-bold ${open ? "hidden" : ""}`}>Orbit</p>
                 <p className={`text-blue-500 text-3xl font-bold transform ${open ? "hidden" : ""}`}>Cloud</p>
             </h1>
-            <div className="">
+            <div className="h-full">
                 <Button text="Dashboard" icon="grid-1x2-fill" open={open} onClick={() => navigationTo('/')} active={pathUrl === '/'} />
                 {/* <Button text="Repositórios" icon="box-seam-fill" onClick={() => navigationTo('/repository')} active={pathUrl === '/repository'}/> */}
                 <Button text="Versões" icon="intersect" open={open} onClick={() => navigationTo('/library')} active={pathUrl === '/library'} />
                 <Button text="Deploys" icon="rocket-takeoff-fill" open={open} onClick={() => navigationTo('/deploy')} active={pathUrl === '/deploy'} />
                 <Button text="Rede" icon="globe2" open={open} onClick={() => navigationTo('/network')} active={pathUrl === '/network'} />
-                <Button text="Cofre" icon="shield-lock-fill" open={open} onClick={() => navigationTo('/cofre')} active={pathUrl === '/cofre'} />
+                <Button text="Cofre" icon="shield-lock-fill" open={open} onClick={() => navigationTo('/vault')} active={pathUrl === '/vault'} />
                 {/* <Button text="Armazenamento" icon="folder-fill" onClick={() => navigationTo('/storage')} active={pathUrl === '/storage'}/> */}
                 {/* <Button text="Planos" icon="award-fill" onClick={() => navigationTo('/subscriptions')} active={pathUrl === '/subscriptions'}/> */}
                 {/* <Button text="Configurações" icon="gear-fill" onClick={() => navigationTo('/settings')} active={pathUrl === '/settings'}/> */}
                 {/* <Button text="Suporte" icon="life-preserver" onClick={() => navigationTo('/support')} active={pathUrl === '/support'}/> */}
             </div>
 
-            <div className="w-full absolute bottom-2">
+            <div className="w-full">
                 {isLoading ? (
                     // Estado de Carregamento (Skeleton simples)
                     <div className="animate-pulse flex items-center space-x-3">
@@ -64,12 +64,13 @@ export default function Nav() {
                 ) : UserData ? (
                     <div className="w-full flex flex-col">
                         <div className="text-slate-500 flex justify-center items-center space-x-3 cursor-pointer hover:text-slate-300 transition-colors">
-                            <p className={`font-bold ${open ? "hidden" : ""} text-white`}>{UserData.name || UserData.username || "Usuário"}</p>
                             {UserData.avatar ? (
                                 <img src={UserData.avatar} alt="Avatar" className="w-9 h-9 rounded-full border border-slate-600" />
+
                             ) : (
                                 <i className="bi bi-person-circle text-3xl"></i>
                             )}
+                            <p className={`font-bold ${open ? "hidden" : ""} text-white`}>{UserData.name || UserData.username || "Usuário"}</p>
                         </div>
                         <Button text="Logout" icon="box-arrow-right" open={open} onClick={() => {
                             document.cookie = "orbit-session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
@@ -77,13 +78,14 @@ export default function Nav() {
                         }} active={false} />
                     </div>
                 ) : (
-                    <Button text="Login" icon="box-arrow-in-right" open={open} onClick={() => navigationTo('/login')} active={pathUrl === '/login'} />
+                    <div className={`w-full flex justify-between ${open ? "flex-col" : ""}`}>
+                        <Button text="Login" icon="door-open" open={open} onClick={() => navigationTo('/login')} active={pathUrl === '/login'} />
+                        <button className={`px-4 flex justify-center text-2xl items-center text-blue-500 cursor-pointer`} onClick={toggleMenu}>
+                            <i className={`bi bi-arrow-bar-${open ? "right" : "left"}`}></i>
+                        </button>
+                    </div>
                 )}
             </div>
-
-            <button className="text-2xl text-blue-500 cursor-pointer absolute -right-3" onClick={toggleMenu}>
-                <i className={`bi bi-arrow-${open ? "right" : "left"}-circle-fill`}></i>
-            </button>
 
             {loading && <div className="position fixed top-0 left-0"><Loading /></div>}
         </nav>
