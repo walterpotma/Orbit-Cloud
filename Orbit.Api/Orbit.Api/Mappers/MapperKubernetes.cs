83938a5380e5868fad6aa782d311717437493241
Waltinho <walterpotma@gmail.com>
2026-02-01 16:51:17 -0300

    arualizando create deployment

--- Orbit.Api/Orbit.Api/Mappers/MapperKubernetes.cs
+++ Orbit.Api/Orbit.Api/Mappers/MapperKubernetes.cs
@@ -258,14 +258,19 @@ namespace Orbit.Api.Mappers
                                 new V1ContainerPort { ContainerPort = request.Port }
                             },
                             // Define limite de recursos (opcional mas recomendado)
-                            //Resources = new V1ResourceRequirements
-                            //{
-                            //    Requests = new Dictionary<string, ResourceQuantity>
-                            //    {
-                            //        { "memory", new ResourceQuantity("128Mi") },
-                            //        { "cpu", new ResourceQuantity("100m") }
-                            //    }
-                            //}
+                            Resources = new V1ResourceRequirements
+                            {
+                                Requests = new Dictionary<string, ResourceQuantity>
+                                {
+                                    { "memory", new ResourceQuantity("100Mi") },
+                                    { "cpu", new ResourceQuantity("50m") }
+                                },
+                                Limits = new Dictionary<string, ResourceQuantity>
+                                {
+                                    { "memory", new ResourceQuantity("512Mi") },
+                                    { "cpu", new ResourceQuantity("100m") }
+                                }
+                            }
                         }
                     }
                         }
