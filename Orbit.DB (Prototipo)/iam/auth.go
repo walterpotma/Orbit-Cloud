e90d3a9c4b26ea709135cb62f93aab6732e11ff2
Walter Brito - DevOps - BNE <walterbrito@bne.com.br>
2026-02-24 16:27:34 -0300

    INIT: Orbit.DB

--- Orbit.DB (Prototipo)/iam/auth.go
+++ Orbit.DB (Prototipo)/iam/auth.go
@@ -1,11 +1,11 @@
 package iam
 
-func ValidateToken(token string) bool {
-    // Lógica simples: "Se token for 123, libera"
-    return token == "super-secret-123"
-}
+// func ValidateToken(token string) bool {
+//     // Lógica simples: "Se token for 123, libera"
+//     return token == "super-secret-123"
+// }
 
-func ValidateUser(user string, senha string) bool {
-    // Lógica simples: "Se usuário for 'admin' e senha for '123', libera"
-    return user == "admin" && senha == "123"
-}
+// func ValidateUser(user string, senha string) bool {
+//     // Lógica simples: "Se usuário for 'admin' e senha for '123', libera"
+//     return user == "admin" && senha == "123"
+// }
--- Orbit.DB (Prototipo)/worker.go
+++ Orbit.DB (Prototipo)/worker.go
@@ -1,17 +1,17 @@
 package main
 
-import (
-    "fmt"
-    "time"
-)
+// import (
+//     "fmt"
+//     "time"
+// )
 
-// StartBackgroundTasks roda "para sempre" sem travar o servidor principal
-func StartBackgroundTasks() {
-    go func() {
-        for {
-            fmt.Println("[Worker] Limpando arquivos antigos...")
-            // Aqui você chamaria storage.Compact()
-            time.Sleep(1 * time. hour) // Dorme por 1 hora
-        }
-    }()
-}
+// // StartBackgroundTasks roda "para sempre" sem travar o servidor principal
+// func StartBackgroundTasks() {
+//     go func() {
+//         for {
+//             fmt.Println("[Worker] Limpando arquivos antigos...")
+//             // Aqui você chamaria storage.Compact()
+//             time.Sleep(1 * time. hour) // Dorme por 1 hora
+//         }
+//     }()
+// }
