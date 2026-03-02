54abf68b2cdc8386e840c7e18f702095b6c13890
Waltinho <walterpotma@gmail.com>
2026-03-02 20:04:09 -0300

    FIX: Retornando metricas individuais dos deployments

--- Orbit.Api/Orbit.Application/DTOs/Prometheus/MetricPoint.cs
+++ Orbit.Api/Orbit.Application/DTOs/Prometheus/MetricPoint.cs
@@ -1,11 +1,23 @@
-﻿public class DeploymentMetricsResponse
-{
-    public List<MetricPoint> TotalUsage { get; set; } = new();
-    public List<PodMetricGroup> PerPodUsage { get; set; } = new();
-}
+﻿using System;
+using System.Collections.Generic;
 
-public class PodMetricGroup
+namespace Orbit.Application.DTOs.Prometheus
 {
-    public string PodName { get; set; }
-    public List<MetricPoint> Data { get; set; } = new();
+    public class MetricPoint
+    {
+        public DateTime Time { get; set; }
+        public double Value { get; set; }
+    }
+
+    public class PodMetricGroup
+    {
+        public string PodName { get; set; } = string.Empty;
+        public List<MetricPoint> Data { get; set; } = new();
+    }
+
+    public class DeploymentMetricsResponse
+    {
+        public List<MetricPoint> TotalUsage { get; set; } = new();
+        public List<PodMetricGroup> PerPodUsage { get; set; } = new();
+    }
 }
