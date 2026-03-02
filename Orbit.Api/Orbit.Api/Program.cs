177be8bf25ab82b0ec3a386969ec6e8456da3b57
Waltinho <walterpotma@gmail.com>
2026-03-02 20:12:33 -0300

    FIX: Retornando metricas individuais dos deployments

--- Orbit.Api/Orbit.Api/Program.cs
+++ Orbit.Api/Orbit.Api/Program.cs
@@ -6,8 +6,6 @@ using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Orbit.Application.Interfaces;
-using Orbit.Application.Interfaces.Services;
 using Orbit.Application.Mappers;
 using Orbit.Application.Services;
 using Orbit.Domain.Interfaces;
