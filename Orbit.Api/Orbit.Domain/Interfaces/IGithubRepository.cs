2adcca2ad5790ad522a6b39bbb929ffe392c7c32
Waltinho <walterpotma@gmail.com>
2026-03-04 20:50:36 -0300

    ADD: Implementando github authentication configuration

--- Orbit.Api/Orbit.Domain/Interfaces/IGithubRepository.cs
+++ Orbit.Api/Orbit.Domain/Interfaces/IGithubRepository.cs
@@ -5,7 +5,7 @@ namespace Orbit.Domain.Interfaces
     public interface IGithubRepository
     {
         #region Github Repositories
-        Task<IEnumerable<DtoGithubReposResponse>> GetUserRepositoriesAsync();
+        // Task<IEnumerable<DtoGithubReposResponse>> GetUserRepositoriesAsync();
         // Task<DtoReposResponse> GetRepositoryByNameAsync(string repoName);
         // Task CloneReposByNameAsync(string repoName);
         #endregion
