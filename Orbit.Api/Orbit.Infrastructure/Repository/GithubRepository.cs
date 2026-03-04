40647250fa0ba294ae61833e05d8c8f3262d92e4
Waltinho <walterpotma@gmail.com>
2026-03-04 20:49:45 -0300

    ADD: Implementando github authentication configuration

--- Orbit.Api/Orbit.Infrastructure/Repository/GithubRepository.cs
+++ Orbit.Api/Orbit.Infrastructure/Repository/GithubRepository.cs
@@ -14,31 +14,31 @@ namespace Orbit.Infrastructure.Repository
         {
             _mapper = mapper;
         }
-        public async Task<IEnumerable<DtoGithubReposResponse>> GetUserRepositoriesAsync()
-        {
-            var userId = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        // public async Task<IEnumerable<DtoGithubReposResponse>> GetUserRepositoriesAsync()
+        // {
+        //     var userId = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-            if (string.IsNullOrEmpty(userId))
-                throw new UnauthorizedAccessException("Usuário não identificado.");
+        //     if (string.IsNullOrEmpty(userId))
+        //         throw new UnauthorizedAccessException("Usuário não identificado.");
 
-            string accessToken = await _githubAuthService.GetTokenForUserAsync(userId);
+        //     string accessToken = await _githubAuthService.GetTokenForUserAsync(userId);
 
-            var httpClient = _httpClientFactory.CreateClient();
+        //     var httpClient = _httpClientFactory.CreateClient();
             
-            var request = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/user/repos?type=owner");
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            request.Headers.UserAgent.Add(new ProductInfoHeaderValue("Orbit-App", "1.0"));
+        //     var request = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/user/repos?type=owner");
+        //     request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        //     request.Headers.UserAgent.Add(new ProductInfoHeaderValue("Orbit-App", "1.0"));
 
-            var response = await httpClient.SendAsync(request);
-            response.EnsureSuccessStatusCode();
+        //     var response = await httpClient.SendAsync(request);
+        //     response.EnsureSuccessStatusCode();
 
-            if (!response.IsSuccessStatusCode)
-                return Enumerable.Empty<DtoReposResponse>();
+        //     if (!response.IsSuccessStatusCode)
+        //         return Enumerable.Empty<DtoReposResponse>();
 
-            var content = await response.Content.ReadAsStringAsync();
+        //     var content = await response.Content.ReadAsStringAsync();
 
-            return _mapper.MapToDto(content);
-        }
+        //     return _mapper.MapToDto(content);
+        // }
         // public Task<DtoGithubReposResponse> GetRepositoryByNameAsync(string repoName)
         // {
 
