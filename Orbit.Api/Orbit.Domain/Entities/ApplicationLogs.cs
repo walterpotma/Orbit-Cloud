4760c218cfcb1ee5b69d6ece2f5f65bd30a43bb0
Walter Brito - DevOps - BNE <walterbrito@bne.com.br>
2026-03-19 15:42:02 -0300

    ADD: Workspace no postgresql

--- Orbit.Api/Orbit.Domain/Entities/ApplicationLogs.cs
+++ Orbit.Api/Orbit.Domain/Entities/ApplicationLogs.cs
@@ -1,9 +1,11 @@
-using Orbit.Domain.Enums; // <--- ADICIONE ISSO
 using Orbit.Domain.Entities;
+using Orbit.Domain.Enums;
+
+namespace Orbit.Domain.Entities;
 
 public class ApplicationLogs : Entity
 {
-    public long ApplicationId { get; private set; }
-    public string Content { get; private set; }
-    public LogType Type { get; private set; }
+    public long ApplicationId { get; set; }
+    public string Content { get; set; }
+    public LogType Type { get; set; }
 }
