d52fcd586719e7f21fb25314bae8ba326700278c
Waltinho <walterpotma@gmail.com>
2026-03-26 20:33:21 -0300

    FIX

--- Orbit.UI/src/shared/layout/logo.tsx
+++ Orbit.UI/src/shared/layout/logo.tsx
@@ -7,8 +7,8 @@ type LogoProps = {
 
 export default function Logo ({className}: LogoProps) {
     return (
-        <div className={`flex justify-center items-center ${className}`}>
-            <img src="/orbit3.png" alt="Logo" className="w-16"/>
+        <div className={`flex justify-center items-center py-2 px-1 rounded-xl bg-white ${className}`}>
+            <img src="/new-orbit.png" alt="Logo" className="w-14"/>
         </div>
     );
 }
