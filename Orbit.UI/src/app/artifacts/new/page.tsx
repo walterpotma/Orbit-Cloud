778c92aeb0f2346c501bcea751f374c85287058e
Waltinho <walterpotma@gmail.com>
2026-04-06 20:01:43 -0300

    FIX: Ajustando temporariamente as urls hotfix da api

--- Orbit.UI/src/app/artifacts/new/page.tsx
+++ Orbit.UI/src/app/artifacts/new/page.tsx
@@ -66,7 +66,7 @@ export default function PipelinePage() {
                 version: formData.version,
             };
 
-            const response = await fetch(`https://orbitcloud.com.br/api/github/deploy`, {
+            const response = await fetch(`https://orbitcloud.com.br/api/build/artifact`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
